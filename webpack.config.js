2cb1865033c80879195064864c9187262b2e56b2
a_hadarovich <hadarovichanna97@gmail.com>
2017-11-01 22:12:20 +0300

    Revert "commit for revert"
    This reverts commit c110ddb9ae33ff0649ec5c93db5e8353c9452683.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
 module.exports = {
+    devtool: 'cheap-module-eval-source-map',
     entry: [
-        'react-hot-loader/patch',
         'webpack-hot-middleware/client',
         'babel-polyfill',
         './src/index'
@@ -14,22 +14,17 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
     ],
     module: { //Обновлено
-
-        rules: [
+        loaders: [ //добавили babel-loader
             {
-                loader: ['babel-loader'],
+                loaders: ['babel-loader'],
                 include: [
                     path.resolve(__dirname, "src"),
                 ],
                 test: /\.js$/,
-                options: {
-                    plugins: [
-                        'react-hot-loader/babel'
-                    ]
-                }
 
             }
         ]
